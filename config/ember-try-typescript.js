8cdbeecce20c6e4aa46ea470536048137f4bf73d
Chris Krycho <hello@chriskrycho.com>
2021-10-14 11:56:27 -0600

    Drop support for Ember <3.24, TS <4.2
    Ember 3.20 and earlier are out of LTS support, and supporting them
    requires extra work for the different modifier manager capabilities.
    The previously-supported TS versions are also well out of date (and
    long past any bug fixes from the TS team). Update to support current
    LTS releases of Ember and TS versions supported in the same range.
    
    Note on dropping tested TS versions: while we did not explicitly test
    against all intermediate versions, they were all implicitly tested by
    the `typescript@next` version, and always passed. Accordingly, users
    can and should safely upgrade to a current/supported version of
    TypeScript *before* adopting the breaking change in Ember versions.
    
    In dropping earlier versions of TypeScript, we can also simplify the
    type checking infrastructure for the Ember Try config: we don't need to
    do any down-leveling, so we can drop the custom configs entirely, and we
    can do our type checking simply by running `tsc --noEmit`.

--- config/ember-try-typescript.js
+++ config/ember-try-typescript.js
@@ -2,21 +2,28 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      command: 'tsc --noEmit --project type-tests/tsconfig-3.6.json',
-      name: 'typescript-3.6',
+      command: 'tsc --noEmit',
+      name: 'typescript-4.2',
       npm: {
-        typescript: '~3.6',
+        typescript: '~4.2',
       },
     },
     {
-      command: 'tsc --noEmit --project type-tests/tsconfig-3.7.json',
-      name: 'typescript-3.7',
+      command: 'tsc --noEmit',
+      name: 'typescript-4.3',
       npm: {
-        typescript: '~3.7',
+        typescript: '~4.3',
       },
     },
     {
-      command: 'tsc --noEmit --project type-tests/tsconfig-next.json',
+      command: 'tsc --noEmit',
+      name: 'typescript-4.4',
+      npm: {
+        typescript: '~4.4',
+      },
+    },
+    {
+      command: 'tsc --noEmit',
       name: 'typescript-next',
       allowedToFail: true,
       npm: {
--- config/ember-try.js
+++ config/ember-try.js
@@ -13,42 +13,18 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.4',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.4.0',
+            'ember-source': '~3.24.0',
           },
         },
       },
       {
-        name: 'ember-lts-3.8',
+        name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.8.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.0',
+            'ember-source': '~3.28.0',
           },
         },
       },
