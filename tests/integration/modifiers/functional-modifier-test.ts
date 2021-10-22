aeb4c90eea18f69aa6d4d27dd686ab35e957bbdf
Chris Krycho <hello@chriskrycho.com>
2021-10-22 17:26:49 -0600

    Ember 4.x compatibility
    - Update to the latest versions of the test infrastructure to work
      correctly against Ember 4. Update two imports to be type-only imports
      (and take a note to fix them upstream in the type definitions).
    - Bump to the currently-targeted `ember-source` and latest `ember-cli`
      to get all fixes for both.
    - Generate a fresh lockfile to get all upstreams using their own latest
      versions of `ember-cli-babel` to avoid miscompilation to the Ember
      global.

--- tests/integration/modifiers/functional-modifier-test.ts
+++ tests/integration/modifiers/functional-modifier-test.ts
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, settled } from '@ember/test-helpers';
-import { TestContext as BaseContext } from 'ember-test-helpers';
+import type { TestContext as BaseContext } from 'ember-test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { modifier } from 'ember-modifier';
 
