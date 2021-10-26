fee0b7fa6409d4c72147f721a97b8999d717936b
Chris Krycho <hello@chriskrycho.com>
2021-10-26 15:21:28 -0600

    Do not run linting as part of tests

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
-    // Add options here
+    'ember-cli-babel': {
+      useBabelConfig: true,
+    },
   });
 
   /*
