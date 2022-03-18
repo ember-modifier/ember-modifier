f8c113940b51071aaee71a5bc40d71eb99056cd8
Chris Krycho <hello@chriskrycho.com>
2022-03-17 18:42:25 -0600

    Bump tests back to type check on TS 4.2+

--- tests/integration/modifiers/class-modifier-test.ts
+++ tests/integration/modifiers/class-modifier-test.ts
@@ -1,10 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import {
-  render,
-  settled,
-  type TestContext as BaseContext,
-} from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
+import type { TestContext as BaseContext } from '@ember/test-helpers';
 import Service, { inject as service } from '@ember/service';
 import { hbs } from 'ember-cli-htmlbars';
 import Modifier, { ModifierArgs } from 'ember-modifier';
--- tests/integration/modifiers/functional-modifier-test.ts
+++ tests/integration/modifiers/functional-modifier-test.ts
@@ -1,10 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import {
-  render,
-  settled,
-  type TestContext as BaseContext,
-} from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
+import type { TestContext as BaseContext } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { modifier } from 'ember-modifier';
 
