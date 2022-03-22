525ec5cc72330f25d92e8f38506bb7dc2161e8e2
Chris Krycho <hello@chriskrycho.com>
2022-03-21 18:35:21 -0600

    Introduce new `modify` hook
    This hook is the future of the class-based modifier API; it replaces
    *all* the non-destruction lifecycle hooks. The new API closely mirrors
    both the existing function-based modifier design and the design of class-based helpers in Ember:
    
        import Modifier from 'ember-modifier';
    
        export default class MyModifier extends Modifier {
          modify(element, positional, named) {
            // ...
          }
        }
    
    This API shift results in several significant improvements compared to
    the previous API:
    
    1.  The signature of `Modifier.modify` matches the signature of a
        function-based signature. Both receive the element and both
        positional and named arguments directly, and in the same positions.
        This means that transitioning from a function- to a class-based
        helper is as simple as using the callback passed to `modifier` as
        the `modify` method on the class. Users no longer need
    
    2.  It now matches the API of a class-based `Helper`, which has only
        the single `compute` method. This is important for rationalizing
        the mental model for using them: Modifiers and helpers are *very*
        similar, with the key difference being that helpers are intended to
        be pure functions which produce values, while modifiers are
        intended to be impure functions which mutate (modify!) a piece of
        DOM and do *not* return values.
    
    3.  This allows us to introduce the desired semantics for modifiers
        *without a breaking change*. Unlike the existing lifecycle hooks,
        `modify` does *not* eagerly consume its arguments. Instead,
        matching the rest of the framework, it only entangles with the
        tracked values it *actually uses*. This means that users should
        take care when migrating their existing modifiers to use the new
        hook.
    
        To maintain exact backwards compatibility, modifier authors would
        need to explicitly use all of the arguments passed to their
        modifiers. Instead, modifier authors should generally cut a new
        major version which has the updated, lazy semantics.
    
    It is a hard error to use `modify` along with any of the existing
    (non-destruction) lifecycle hooks. This allows us to guarantee that
    `modify` maintains the standard auto-tracking semantics as described
    above.
    
    A future PR will deprecate the existing lifecycle hooks; users will
    need to begin migrating to the new design. By introducing `modify` this
    way, however, users can migrate their existing modifiers piecemeal.

--- addon/-private/class/modifier-manager.ts
+++ addon/-private/class/modifier-manager.ts
@@ -1,14 +1,48 @@
 import { capabilities } from '@ember/modifier';
 import { gte } from 'ember-compatibility-helpers';
 import { set } from '@ember/object';
-import { destroy, registerDestructor } from '@ember/destroyable';
+import {
+  destroy,
+  registerDestructor,
+  associateDestroyableChild,
+} from '@ember/destroyable';
 
-import ClassBasedModifier from './modifier';
+import ClassBasedModifier, { _implementsModify } from './modifier';
 import { ArgsFor, ElementFor } from 'ember-modifier/-private/signature';
 import { consumeArgs, Factory, isFactory } from '../compat';
 
 function destroyModifier<S>(modifier: ClassBasedModifier<S>): void {
   modifier.willDestroy();
+}
+
+/**
+ * The state bucket used throughout the life-cycle of the modifier. Basically a
+ * state *machine*, where the framework calls us with the version we hand back
+ * to it at each phase. The two states are the two `extends` versions of this
+ * below.
+ * @internal
+ */
+interface State<S> {
+  instance: ClassBasedModifier<S>;
+  element: ElementFor<S> | null;
+}
+
+/**
+ * The `State` after calling `createModifier`, and therefore the state available
+ * at the start of `InstallModifier`.
+ * @internal
+ */
+interface CreateState<S> extends State<S> {
+  element: null;
+}
+
+/**
+ * The `State` after calling `installModifier`, and therefore the state
+ * available in all subsequent `updateModifier` calls.
+ * @internal
+ */
+interface InstalledState<S> extends State<S> {
+  element: ElementFor<S>;
 }
 
 export default class ClassBasedModifierManager<S> {
@@ -21,43 +55,76 @@ export default class ClassBasedModifierManager<S> {
       | Factory<typeof ClassBasedModifier>
       | typeof ClassBasedModifier,
     args: ArgsFor<S>
-  ): ClassBasedModifier<S> {
+  ): CreateState<S> {
     const Modifier = isFactory(factoryOrClass)
       ? factoryOrClass.class
       : factoryOrClass;
 
     const modifier = new Modifier(this.owner, args);
 
+    const state: CreateState<S> = {
+      instance: modifier,
+      element: null,
+    };
+
+    // Since we have created a state bucket to hand around, we need to make its
+    // destruction trigger the destruction of the modifier. (Ember will
+    // automatically call `destroy` on the state bucket returned from the
+    // `createModifier` hook for precisely this reason.)
+    associateDestroyableChild(state, modifier);
     registerDestructor(modifier, destroyModifier);
 
-    return modifier;
+    return state;
   }
 
   installModifier(
-    instance: ClassBasedModifier<S>,
+    state: State<S>,
     element: ElementFor<S>,
     args: ArgsFor<S>
   ): void {
+    const { instance } = state;
     instance.element = element;
+    state.element = element;
 
-    if (gte('3.22.0')) {
+    // The `consumeArgs()` call backwards compatibility on v3 for the deprecated
+    // legacy lifecycle hooks (`didInstall`, `didReceiveArguments`, and
+    // `didUpdateArguments`), which accidentally had eager consumption semantics
+    // prior to Ember 3.22. The new, recommended `modify` hook has the updated
+    // lazy semantics associated with normal auto-tracking.
+    const implementsModify = _implementsModify(instance);
+    if (gte('3.22.0') && !implementsModify) {
       consumeArgs(args);
     }
 
-    instance.didReceiveArguments();
-    instance.didInstall();
+    if (implementsModify) {
+      instance.modify(element, args.positional, args.named);
+    } else {
+      instance.didReceiveArguments();
+      instance.didInstall();
+    }
   }
 
-  updateModifier(instance: ClassBasedModifier<S>, args: ArgsFor<S>): void {
-    // TODO: this should be an args proxy
-    set(instance, 'args', args);
+  updateModifier(state: InstalledState<S>, args: ArgsFor<S>): void {
+    const { instance } = state;
 
-    if (gte('3.22.0')) {
+    set(instance, 'args', args); // TODO: remove aat 4.0
+
+    // The `consumeArgs()` call backwards compatibility on v3 for the deprecated
+    // legacy lifecycle hooks (`didInstall`, `didReceiveArguments`, and
+    // `didUpdateArguments`), which accidentally had eager consumption semantics
+    // prior to Ember 3.22. The new, recommended `modify` hook has the updated
+    // lazy semantics associated with normal auto-tracking.
+    const implementsModify = _implementsModify(instance);
+    if (gte('3.22.0') && !implementsModify) {
       consumeArgs(args);
     }
 
-    instance.didUpdateArguments();
-    instance.didReceiveArguments();
+    if (implementsModify) {
+      instance.modify(state.element, args.positional, args.named);
+    } else {
+      instance.didUpdateArguments();
+      instance.didReceiveArguments();
+    }
   }
 
   destroyModifier(instance: ClassBasedModifier): void {
