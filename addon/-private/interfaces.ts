dd13e872e7cb83c1b9a8fef851aa83758f69be6e
Chris Krycho <hello@chriskrycho.com>
2022-03-28 14:51:55 -0600

    Re-export `ModifierArgs` from its original location
    This will make sure that any existing `.d.ts` files generated by authors
    of addons continue to work correctly if they reference this path.

---
+++ addon/-private/interfaces.ts
@@ -0,0 +1,4 @@
+// This re-export is here to make sure any existing type definitions which refer
+// to this path continue to work on 3.x.
+/** @deprecated */
+export type { ModifierArgs } from './signature';
