fb12a5b7f5bd67e7eb674a796180f6b63dde7d50
Krystan HuffMenne <krystan@tilde.io>
2023-02-22 13:14:06 -0800

    Revert peerDependencies change

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -3,10 +3,46 @@
 const getChannelURL = require('ember-source-channel-url');
 const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
+// These are needed for ember-source < 4.8, when preview types were first shipped
+const emberTypesPackages = {
+  '@types/ember-resolver': '^5.0.13',
+  '@types/ember__application': '^4.0.5',
+  '@types/ember__owner': '^4.0.3',
+};
+
 module.exports = async function () {
   return {
     useYarn: true,
     scenarios: [
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            '@types/ember-qunit': '^5.0.2',
+            'ember-qunit': '^5.1.5',
+            'ember-source': '~3.24.0',
+            ...emberTypesPackages,
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+            ...emberTypesPackages,
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+            ...emberTypesPackages,
+          },
+        },
+      },
       {
         name: 'ember-lts-4.8',
         npm: {
